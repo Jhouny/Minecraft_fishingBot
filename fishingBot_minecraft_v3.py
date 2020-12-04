f45becc96f47eaed7d189cbd199f46ffc6921f07
Jhouny <jhonyaraujo9121@gmail.com>
2020-12-04 10:34:38 -0300

    v3: working

--- fishingBot_minecraft_v3.py
+++ fishingBot_minecraft_v3.py
@@ -2,9 +2,10 @@ import pyscreenshot as ImGrab
 from PIL import Image
 import itertools
 from time import time, sleep
-from numba import jit, cuda
+#from numba import jit, cuda
 from math import sqrt
 import pyautogui as pag
+import keyboard
 
 #@jit(target ="cuda")
 def Score_inSum(c, target_index):
@@ -26,7 +27,7 @@ def Score_inPercentage(c, target_index):
 
     s0 = color[target_index]
     if sum >= 10:
-        perc = (s0/sum)*1080
+        perc = (s0/sum)*100
     else:
         perc = 0
 
@@ -59,6 +60,7 @@ def Screenshot(resizePercentage, box):
 def findTarget(img, target_index):
     max = (0,0,0)
     pos = [0,0]
+    score = 0
     pix = img.load()
 
     for x in range(img.size[0]):
@@ -67,21 +69,22 @@ def findTarget(img, target_index):
             if score_pixel > Score_inPercentageNLuminosity(max, target_index):
                 max = pix[x, y]
                 pos = [x, y]
+                score = score_pixel
 
-    return max, pos
+    return max, pos, score
 
 def measureChange(im1, im2, target_index, resizePercentage, threshold):
-    c1, target1 = findTarget(im1, target_index)
-    c2, target2 = findTarget(im2, target_index)
+    c1, target1, sc1 = findTarget(im1, target_index)
+    c2, target2, sc2 = findTarget(im2, target_index)
     d = sqrt((target2[0]-target1[0])**2+(target2[1]-target1[1])**2)
     standardizedPixelDistance = d*(1/resizePercentage)
     if standardizedPixelDistance >= threshold:
-        return True, standardizedPixelDistance
+        return True, standardizedPixelDistance, target2, sc2
     else:
-        return False, standardizedPixelDistance
+        return False, standardizedPixelDistance, target2, sc2
 
 
-sleep(2)
+sleep(3)
 
 #Choose color to be found with maximum value in image
 target = "Red"
@@ -89,27 +92,34 @@ opt = ["Red", "Green", "Blue"]
 ind = opt.index(target)
 threshold = 100
 state = 1
-resizePercentage = 0.2
+resizePercentage = 0.1
 last = Screenshot(resizePercentage, (0,0, 1920, 1080))
 image = Screenshot(resizePercentage, (0,0, 1920, 1080))
 t1 = time()
 t2 = time()
 while state:
-    print("Delay: ", (t2-t1))
+    print("\nDelay: ", (t2-t1))
     t1 = time()
     last = image
     image = Screenshot(resizePercentage, (0,0, 1920, 1080))
-    activation, d = measureChange(last, image, ind, resizePercentage, threshold)
-    if activation == True:
-        print("Distance bigger than threshold = ", d)
-        pag.click(button="right", clicks=2, interval=1.5)
+    activation, d, pos, score = measureChange(last, image, ind, resizePercentage, threshold)
+    #pos = [a*(1/resizePercentage) for a in pos]
+    if keyboard.is_pressed("k"):
         state = 0
+
+    if activation == True:
+        print("Distance bigger than threshold = ", d, "\nObject detected at ", pos)
+        print("Score: ", score)
+        pag.click(button="right", clicks=2, interval=1.5, x=960, y=554)
+        sleep(1)
+        #state = 0
     else:
-        print("Distance is below threshold")
+        print("Distance is below threshold and object is at ", pos)
+        print("Score: ", score)
     t2 = time()
 
-last.save("/home/jhony_ma/Downloads/test.png")
-image.save("/home/jhony_ma/Downloads/test2.png")
+#last.save("/home/jhony_ma/Downloads/test.png")
+#image.save("/home/jhony_ma/Downloads/test2.png")
 
 
 """
